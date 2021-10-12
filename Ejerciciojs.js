0c9c4e60e1bea5e29b006349a0aa2042ab7be41f
fnsanabria <florn_sanabria@hotmail.com>
2021-10-11 22:55:35 -0300

    First project

---
+++ Ejerciciojs.js
@@ -0,0 +1,21 @@
+<html>
+<head>
+<script>
+
+var primera = 40
+var segunda = 16
+var sumatotal = primera+segunda
+
+document.write(<h1>"El resultado de la suma de las dos variables es: " + sumatotal</h1>)
+
+
+</script>
+
+</head>
+<body>
+
+
+
+</body>
+
+</html>
---
+++ Multiplicacionjs.js
@@ -0,0 +1,23 @@
+<>
+<html>
+
+    <head>
+        <h1>Tablas de Multiplicar</h1>
+        <script>
+
+            document.write("La tabla del 11")
+
+            for(var i=1; i < = 10; i++){
+            document.write(11 + "x " + i + "= " + 11 * i)
+            }
+
+        </script>
+
+    </head>
+<body>
+
+
+
+</body>
+
+</html></>
